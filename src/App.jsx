47d3033c23a2a5da7630dfe0f63f4a1f42d3b613
gabriiellrss10 <gabrielrodrigossantana@gmail.com>
2024-12-09 12:09:15 -0300

    consetei o menu3

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Footer from './Components/footer.jsx';
 function App() {
 
 
+
   const Layout = ({ children }) => {
     const location = useLocation();
     const atual = "/lastfall"; 
@@ -26,6 +27,7 @@ function App() {
       </>
     );
   };
+  
   
 
   return (
